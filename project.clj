511108f5bb0df085e5734858673e0db172b16ce7
Rob Stenson <rob.stenson@gmail.com>
2015-04-24 16:07:04 -0400

    update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject hieronymus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject hieronymus "0.1.1-SNAPSHOT"
+  :description "Weird characters"
+  :url "http://github.com/stenson/hieronymus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
